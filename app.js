787f50f88dd6d90d92d2343cfe0dbeed41fbe7bb
XxkuppiexX <37938611+XxkuppiexX@users.noreply.github.com>
2018-05-12 17:22:32 +0200

    bot files

---
+++ app.js
@@ -0,0 +1,130 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+const config = require("./config.json");
+
+  
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  // Let's go with a few common example commands! Feel free to delete or change those.
+
+  if(command === "say") {
+    if(!message.member.roles.some(r=>["☄️ Galaxy CEO ☄️", "⚙️ Head-Support ⚙️", "bot perms"].includes(r.name)) )
+    return message.reply("Sorry je hebt hier geen perms voor :(");
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
+	
+if(command === "set") {
+  if(!message.member.roles.some(r=>["☄️ Galaxy CEO ☄️"].includes(r.name)) )
+  return message.reply("Je hebt hier geen perms voor");
+  const sayMessage = args.join(" ");
+  message.delete().catch(O_o=>{});
+  client.user.setActivity(sayMessage);
+}
+	
+	
+  if(command === "warn") {
+    if(!message.member.roles.some(r=>["☄️ Galaxy CEO ☄️", "⚙️ Head-Support ⚙️", "bot perms"].includes(r.name)) )
+    return message.reply("Sorry je hebt hier geen perms voor :(");
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send("Dit is een waarschuwing wegens: "(sayMessage))
+  }
+
+  
+  if(command === "kick") {
+    // This command must be limited to mods and admins. In this example we just hardcode the role names.
+    // Please read on Array.some() to understand this bit: 
+    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+    if(!message.member.roles.some(r=>["beheer"].includes(r.name)) )
+      return message.reply("je kan geen !kick vraag staff om hulp");
+    
+    // Let's first check if we have a member and if we can kick them!
+    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+    // We can also support getting the member by ID, which would be args[0]
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("je moet @ gebruiken");
+    if(!member.kickable) 
+      return message.reply("hu, ik kan hem niet kicken heb ik eigenlijk wel kick perms??");
+    
+    // slice(1) removes the first part, which here should be the user mention or ID
+    // join(' ') takes all the various parts to make it a single string.
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "hij heeft geen reden ingevult";
+    
+    // Now, time for a swift kick in the nuts!
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} kan ik niet kicken om: ${error}`));
+    message.reply(`${member.user.tag} is gekickt door ${message.author.tag} omdat: ${reason}`);
+
+  }
+  
+  if(command === "ban") {
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+    // In the real world mods could ban too, but this is just an example, right? ;)
+    if(!message.member.roles.some(r=>["⚙️ Head-Support ⚙️", "☄️ Galaxy CEO ☄️", "bot perms"].includes(r.name)) )
+      return message.reply("sorry je kan geen !ban vraag staff om hulp");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("je moet @ gebruiken");
+    if(!member.bannable) 
+      return message.reply("hu, ik kan hem niet bannen heb ik eigenlijk wel ban perms??");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "geen reden ingevult";
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} kan ik niet bannen om: ${error}`));
+    message.reply(`${member.user.tag} is geband door ${message.author.tag} omdat: ${reason}`);
+  }
+  
+  if(command === "clear") {
+    if(!message.member.roles.some(r=>["beheer"].includes(r.name)) )
+    return message.reply("je kan geen !clear vraag aan een CEO of head-support om hulp");
+    // This command removes all messages from all users in the channel, up to 100.
+    
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("voer een nummer tussen de 2 en 100 in");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`ik kan de message's niet delete omdat: ${error}`));
+  }
+});
+
+
+client.login(process.env.BOT_TOKEN);
